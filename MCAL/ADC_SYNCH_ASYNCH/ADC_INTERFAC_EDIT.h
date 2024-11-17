457fd028f0d4d3cf3b05687e57d2bc308742eaa1
Omar Khaled/Embedded Systems/C programming <150465142+Unvisable@users.noreply.github.com>
2024-11-17 19:39:14 +0200

    Add files via upload

---
+++ MCAL/ADC_SYNCH_ASYNCH/ADC_INTERFAC_EDIT.h
@@ -0,0 +1,26 @@
+#ifndef ADC_INTERFACE_H
+#define ADC_INTERFACE_H
+
+
+typedef struct{
+	uint8 ChainSize;
+	uint8* ChannelArr;
+	uint16* ResultArr;
+	void(*NotificationFunc)(void);
+}ADC_Chain_t;
+
+typedef struct{
+       uint8 ADC_InitChainSize;
+	   uint8* ADC_InitChannelArr;
+	   uint8 ADC_InitSetResolution;
+}ADC_Init_Chain_t;
+
+uint8 ADC_voidInit(ADC_Init_Chain_t* ADCCopy_Object);
+
+uint8 ADC_u8StartSingleConversionSynch(uint8 Copy_u8Channel, uint16* Copy_pu16Result);
+
+uint8 ADC_u8StartSingleConversionAsynch(uint8 Copy_u8Channel, uint16* Copy_pu16Result, void (*Copy_pvNotificationFunc)(void));
+
+uint8 ADC_u8StartChainConversionAsynch(ADC_Chain_t* Copy_Object);
+
+#endif
